9d2b1923b3d4c180879a44d5ec78214c299d66c1
JackRose12 <128778538+JackRose12@users.noreply.github.com>
2025-12-01 11:02:04 +0530

    Update Temp.c

--- Temp.c
+++ Temp.c
@@ -1,19 +1,45 @@
 #include <stdio.h>
 
 int main() {
-    int n, i, j;
-    int num = 1;
+    int n, i, key;
+    int found = 0;
 
-    // Taking user input
-    printf("Enter number of rows: ");
+    // Input number of elements
+    printf("Enter number of elements: ");
     scanf("%d", &n);
 
-    for (i = 1; i <= n; i++) {           // loop for rows
-        for (j = 1; j <= i; j++) {       // loop for printing numbers
-            printf("%d", num);
-            num++;
+    int arr[n];
+    int positions[n];   // to store duplicate positions
+    int posCount = 0;
+
+    // Input array elements
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Input key value
+    printf("Enter the key value to search: ");
+    scanf("%d", &key);
+
+    // Linear Search with duplicate detection
+    for (i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            positions[posCount] = i + 1; // store 1-based index
+            posCount++;
+            found = 1;
         }
-        printf("\n");                    // move to next line
+    }
+
+    // Output result
+    if (!found) {
+        printf("Key %d not found.\n", key);
+    } else {
+        printf("Key %d found at %d position(s):\n", key, posCount);
+        for (i = 0; i < posCount; i++) {
+            printf("%d ", positions[i]);
+        }
+        printf("\n");
     }
 
     return 0;
