e5630b75f43217a1fbfd48a927779611a94ddd69
JackRose12 <128778538+JackRose12@users.noreply.github.com>
2025-12-01 11:19:30 +0530

    Update Temp.c

--- Temp.c
+++ Temp.c
@@ -2,18 +2,26 @@
 
 int main() {
     int n, i, j;
+    int start, val;
 
-    // taking user input
     printf("Enter number of rows: ");
     scanf("%d", &n);
 
-    for (i = 1; i <= n; i++) {        // rows
-        for (j = 1; j <= i; j++) {    // columns
-            if (j % 2 == 1)
-                printf("1");          // odd position → 1
-            else
-                printf("0");          // even position → 0
+    for (i = 1; i <= n; i++) {
+
+        // start value changes every 2 rows
+        if ( ((i - 1) / 2) % 2 == 0 )
+            start = 1;
+        else
+            start = 0;
+
+        val = start;
+
+        for (j = 1; j <= i; j++) {
+            printf("%d", val);
+            val = 1 - val;  // alternate 1 and 0
         }
+
         printf("\n");
     }
 
