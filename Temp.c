24f845dfa7d4138eda678942d65913b906d703c4
JackRose12 <128778538+JackRose12@users.noreply.github.com>
2025-12-01 13:04:22 +0530

    Update Temp.c

--- Temp.c
+++ Temp.c
@@ -1,26 +1,35 @@
-#include <stdio.h>
-
-int main() {
-    int n, i, j;
-
-    // user input
-    printf("Enter number of rows: ");
-    scanf("%d", &n);
-
-    for (i = 1; i <= n; i++) {
-
-        // print spaces
-        for (j = 1; j <= n - i; j++) {
-            printf(" ");
-        }
-
-        // print numbers
-        for (j = 1; j <= i; j++) {
-            printf("%d", j);
-        }
-
-        printf("\n");
-    }
-
-    return 0;
+#include<stdio.h>
+#define rows
+#define cols
+void main(){
+	int a[rows][cols] ,b[cols][rows],i,j;
+	printf("Enter values in the 2-D array");
+	for (i=0,i<rows,i++){
+		for(j=0,j<cols,j++){
+			scanf("%d",&a[i][j]);
+		}
+		
+	}
+	printf("1st matrix is:/n");
+	for (i=0;i<rows,i++){
+		for(j=0,j<cols,j++){
+			printf("%d\t",a[i][j]);
+		}
+		printf("\n");
+		
+	}
+	for(i=0;i<cols;i++){
+		for(j=0;j<rows;j++){
+			b[i][j]=a[j][i];
+		}
+	}
+	printf("Transpose matrix is:\n");
+	for(i=0;i<cols;i++){
+		for(j=0;j<rows;j++){
+			printf("%d\t",b[i][j]);
+		}
+		printf("\n");
+		
+	}
+		
 }
