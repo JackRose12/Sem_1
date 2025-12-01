428ac921d2d9b308e43454d56378bbbd9382d04c
JackRose12 <128778538+JackRose12@users.noreply.github.com>
2025-12-01 11:52:25 +0530

    Update Temp.c

--- Temp.c
+++ Temp.c
@@ -7,17 +7,18 @@ int main() {
     printf("Enter number of rows: ");
     scanf("%d", &n);
 
-    // print hollow triangle
     for (i = 1; i <= n; i++) {
-        for (j = 1; j <= i; j++) {
 
-            // boundary conditions
-            if (j == 1 || j == i || i == n) {
-                printf("*");   // boundary stars
-            } else {
-                printf(" ");   // inside space
-            }
+        // print spaces
+        for (j = 1; j <= n - i; j++) {
+            printf(" ");
         }
+
+        // print numbers
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
         printf("\n");
     }
 
