ac64220c4ef524e1e9656d82d4ed370a2aa6f9da
JackRose12 <128778538+JackRose12@users.noreply.github.com>
2026-01-05 11:16:11 +0530

    Create 21.c

---
+++ 21.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+// Recursive function to find GCD
+int gcd(int a, int b)
+{
+    if(b == 0)
+        return a;          // Base case
+    else
+        return gcd(b, a % b);   // Recursive call
+}
+
+int main()
+{
+    int num1, num2;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("GCD of %d and %d is: %d\n", num1, num2, gcd(num1, num2));
+
+    return 0;
+}
